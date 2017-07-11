fa7d8a3a3ba00fdfdeecb959f024cabaf7eda710
Cindy Fan Chiang <hfanc001@ucr.edu>
2017-07-11 06:37:08 -0700

    add new activity that shows input message

--- app/src/main/java/edustanprojectdrupalqvsclab_home/ucr/ece/httpwww/iotcontroller/MainActivity.java
+++ app/src/main/java/edustanprojectdrupalqvsclab_home/ucr/ece/httpwww/iotcontroller/MainActivity.java
@@ -1,29 +1,26 @@
 package edustanprojectdrupalqvsclab_home.ucr.ece.httpwww.iotcontroller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
-
+    public static final String EXTRA_MESSAGE = "edustanprojectdrupalqvsclab_home.ucr.ece.httpwww.iotcontroller.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    /** Called when the user taps the send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
 }
