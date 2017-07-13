2705b41d3fbcc0394fcbd458f6af6a80755673e3
Cindy Fan Chiang <hfanc001@ucr.edu>
2017-07-13 02:09:42 -0700

    Add toggle switch

--- app/src/main/java/edustanprojectdrupalqvsclab_home/ucr/ece/httpwww/iotcontroller/DisplayMessageActivity.java
+++ app/src/main/java/edustanprojectdrupalqvsclab_home/ucr/ece/httpwww/iotcontroller/DisplayMessageActivity.java
@@ -3,10 +3,14 @@ package edustanprojectdrupalqvsclab_home.ucr.ece.httpwww.iotcontroller;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
+   //private ToggleButton toggleButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,10 +21,27 @@ public class DisplayMessageActivity extends AppCompatActivity {
         String switch_name = intent.getStringExtra(MainActivity.SWITCH_NAME);
         String switch_location = intent.getStringExtra(MainActivity.SWITCH_LOCATION);
 
-        //Capture the layout's TextvView and set the string as its text
+        //Capture the layout's TextView and set the string as its text
         TextView view_name = (TextView) findViewById(R.id.display_name);
         view_name.setText(switch_name);
         TextView view_location = (TextView) findViewById(R.id.display_location);
         view_location.setText(switch_location);
+
+        ToggleButton toggle = (ToggleButton) findViewById(R.id.display_status);
+        toggle.setChecked(MainActivity.SWITCH_STATUS);
+
+        MainActivity.SWITCH_STATUS = toggle.isChecked();
+        /*toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                MainActivity.SWITCH_STATUS = isChecked;
+            }
+        });
+*/
+    }
+
+    public void onToggleClicked (View view) {
+        ToggleButton toggle = (ToggleButton) findViewById(R.id.display_status);
+        MainActivity.SWITCH_STATUS = toggle.isChecked();
     }
 }
--- app/src/main/java/edustanprojectdrupalqvsclab_home/ucr/ece/httpwww/iotcontroller/MainActivity.java
+++ app/src/main/java/edustanprojectdrupalqvsclab_home/ucr/ece/httpwww/iotcontroller/MainActivity.java
@@ -4,13 +4,17 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import static android.R.id.toggle;
 
 public class MainActivity extends AppCompatActivity {
     public static final String SWITCH_NAME = "edustanprojectdrupalqvsclab_home.ucr.ece.httpwww.iotcontroller.NAME";
     public static final String SWITCH_LOCATION = "edustanprojectdrupalqvsclab_home.ucr.ece.httpwww.iotcontroller.LOCATION";
-    //public static final boolean SWITCH_STATUS = "edustanprojectdrupalqvsclab_home.ucr.ece.httpwww.iotcontroller.STATUS";
+    public static boolean SWITCH_STATUS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +27,15 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText get_name = (EditText) findViewById(R.id.input_name);
         EditText get_location = (EditText) findViewById(R.id.input_location);
-        //EditText get_status = ()
+
         String name = get_name.getText().toString();
         String location = get_location.getText().toString();
         intent.putExtra(SWITCH_NAME, name);
         intent.putExtra(SWITCH_LOCATION, location);
         startActivity(intent);
+
+        ToggleButton toggle = (ToggleButton) findViewById(R.id.input_status);
+        SWITCH_STATUS = toggle.isChecked();
     }
 }
+
